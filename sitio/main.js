c0ef256701616d40e0a412f1029131d375d0897b
Eduardo-Riera <juanpe752@gmail.com>
2022-10-26 20:59:19 -0300

    Modificaciones preliminares

--- sitio/main.js
+++
@@ -1,5 +0,0 @@
-var div = document.getElementById("target");
-        div.onload = function() {
-            div.style.height =
-              div.contentWindow.document.body.scrollHeight +'px';
-        }
